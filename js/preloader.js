dd06474b6f65bc33aa4cf51f1d58fd6732e66656
DMinh <taducminh2004@gmail.com>
2021-02-28 18:55:33 +0700

    fix js

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
 const preloader = document.getElementById('preloader');
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     setTimeout(function(){
         preloader.classList.add('loaded');
     }, 1300);
